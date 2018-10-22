7323179bc9d477c33ed9e31cdbc54da89a20af9f
Lucas-Rufino <lar@cin.ufpe.br>
2018-10-21 22:32:05 -0300

    testes

--- tests.py
+++ tests.py
@@ -26,41 +26,62 @@ pattern = sources
 # 			print(file + " - " + cmd + " - " + str(size))
 # 			os.system("time " + cmd + " -c \"" + pattern[0:size] + "\" " + path + file)
 
-with open('pattern-' + file + '.txt', 'w') as fl:
-	for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
-		for size in list(range(bgn, end, stp))[::-1]:
-			fl.write(pattern[0:size] + '\n')
+# TESTE COM MULTIPLOS PADRÕES EM UM ARQUIVO
+# with open('pattern-' + file + '.txt', 'w') as fl:
+# 	for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
+# 		for size in list(range(bgn, end, stp))[::-1]:
+# 			fl.write(pattern[0:size] + '\n')
+# 
+# for algo in ["ShiftOr", "WuManber", "Ukkonen", "AhoCorasik"]:
+# 	for i in range(1, 24):
+# 		with open('pattern-' + file + '.txt', 'w') as fl:
+# 			count = 0;
+# 			for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
+# 				for size in list(range(bgn, end, stp))[::-1]:
+# 					if count < i:
+# 						fl.write(pattern[0:size] + '\n')
+# 						count += 1
+# 					else:
+# 						break
+# 		print("- - - - - - - - - - - ")
+# 		print(file + " - " + algo + " - " + str(i))
+# 		os.system("time pmt -c -a " + algo + " -p pattern-" + file + ".txt " + path + file)
+# for cmd in ["grep", "ggrep"]:
+# 	for i in range(1, 24):
+# 		with open('pattern-' + file + '.txt', 'w') as fl:
+# 			count = 0;
+# 			for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
+# 				for size in list(range(bgn, end, stp))[::-1]:
+# 					if count <= i:
+# 						fl.write(pattern[0:size] + '\n')
+# 						count += 1
+# 					else:
+# 						break
+# 		print("- - - - - - - - - - - ")
+# 		print(file + " - " + cmd + " - " + str(i))
+# 		os.system("time " + cmd + " -c -f pattern-" + file + ".txt " + path + file)
 
-for algo in ["ShiftOr", "WuManber", "Ukkonen", "AhoCorasik"]:
-	for i in range(1, 24):
-		with open('pattern-' + file + '.txt', 'w') as fl:
-			count = 0;
-			for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
-				for size in list(range(bgn, end, stp))[::-1]:
-					if count < i:
-						fl.write(pattern[0:size] + '\n')
-						count += 1
-					else:
-						break
-		print("- - - - - - - - - - - ")
-		print(file + " - " + algo + " - " + str(i))
-		os.system("time pmt -c -a " + algo + " -p pattern-" + file + ".txt " + path + file)
-for cmd in ["grep", "ggrep"]:
-	for i in range(1, 24):
-		with open('pattern-' + file + '.txt', 'w') as fl:
-			count = 0;
-			for bgn, end, stp in [(1, 10, 1), (10, 50, 5), (50, 101, 10)][::-1]:
-				for size in list(range(bgn, end, stp))[::-1]:
-					if count <= i:
-						fl.write(pattern[0:size] + '\n')
-						count += 1
-					else:
-						break
-		print("- - - - - - - - - - - ")
-		print(file + " - " + cmd + " - " + str(i))
-		os.system("time " + cmd + " -c -f pattern-" + file + ".txt " + path + file)
+pitches = "977SJLJLGJLJEVJLJ"
+sources = "art = (0xFFFF - ("
+dblpxml = "The Design and De"
+dna = "GATCAATGAGGTGGACA" 
+proteins = "MGKSEGPVGMVESAGRA" 
+english = "of the intellectu"
 
+path = "data/"
+file = "pitches"
+pattern = pitches
 
+for algo in ["WuManber", "Ukkonen"]:
+	for edit in range(0, 16):
+			print("- - - - - - - - - - - ")
+			print(file + " - " + algo + " - " + str(edit))
+			os.system("time pmt -c -a " + algo + " -e " + str(edit) + " \"" + pattern + "\" " + path + file)
+for cmd in ["agrep"]:
+	for edit in range(0, 16):
+			print("- - - - - - - - - - - ")
+			print(file + " - " + cmd + " - " + str(edit))
+			os.system("time " + cmd + " -c  -" + str(edit) + " \"" + pattern + "\" " + path + file)
 
 
 
